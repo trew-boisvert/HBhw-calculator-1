09e8aa6c33b5d71389e1f3a582b4266ba451aa9c
Trew <trewboisvert@gmail.com>
2021-01-06 16:54:53 -0800

    Created add_cubes function and added Docstring to add_mult function

--- arithmetic.py
+++ arithmetic.py
@@ -36,4 +36,9 @@ def mod(num1, num2):
     return num1 % num2
 
 def add_mult(num1, num2, num3):
+    """Return the result of adding the first two numbers, and then multiplying the total by the third number."""
     return (num1 + num2) * num3
+
+def add_cubes(num1, num2):
+    """Return the sum of the cubes of each number""" 
+    return (num1 ** 3) + (num2 ** 3)
